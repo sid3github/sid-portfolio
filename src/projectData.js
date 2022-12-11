f84d48617a90590e72e82b8e2ca3e53e74e0482d
Siddharth Padwal <siddharthpadwal@Siddharths-MacBook-Pro.local>
2022-12-12 00:46:08 +0530

    added image name changes

--- src/projectData.js
+++ src/projectData.js
@@ -1,19 +1,19 @@
 const projectInfo = [
   {
     project_name: "Music Player",
-    project_image_url: "project_1.png",
+    project_image_url: "./assets/images/project_one.png",
     project_code_url: "https://github.com/sid3github/vue-music-app",
     project_preview_url: "https://goofy-mayer-78ec5d.netlify.app/",
   },
   {
     project_name: "Quiz App",
-    project_image_url: "project_2.png",
+    project_image_url: "./assets/images/project_two.png",
     project_code_url: "https://github.com/sid3github/vue-quiz",
     project_preview_url: "https://vue3quiz.netlify.app/",
   },
   {
     project_name: "Movie Database",
-    project_image_url: "project_3.png",
+    project_image_url: "./assets/images/project_three.png",
     project_code_url: "https://github.com/sid3github/vue-movie-search",
     project_preview_url: "https://vigorous-dijkstra-abb2c3.netlify.app",
   },
