828d6f21e4f48db0597d92d73361dc610f914527
Siddharth Padwal <sid3github@gmail.com>
2026-09-21 18:48:04 +0530

    Emit static build to configured dist directory

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
